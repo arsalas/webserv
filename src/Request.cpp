87af4d8f802e057456fd62ae2d2058c79283e4af
aliciamurma <102992210+aliciamurma@users.noreply.github.com>
2023-03-12 19:21:43 +0100

    Request

--- src/Request.cpp
+++ src/Request.cpp
@@ -27,6 +27,43 @@ std::vector<std::string>    vectorSplit(std::string file, std::string delimiter)
     return (auxVector);
 }
 
+bool    isHyphenDigit(std::string str)
+{
+    int c = 0;
+    while (str[c] == '-' || isdigit(str[c]))
+        c++;
+    if (!str.empty() && str[c] == '\0')
+    {
+        return (1);
+    }
+    return (0);
+}
+
+
+void    Request::setFormData(std::vector<std::string>::iterator itVector, std::vector<std::string> auxVector, std::vector<std::string>::iterator endVector)
+{
+    (void) endVector;
+    if (isHyphenDigit(*itVector))
+    {
+        std::vector<std::string> newVector;
+        std::vector<std::string>::iterator firstIter;
+        std::vector<std::string>::iterator secondtIter;
+        for (; itVector < endVector; itVector++)
+        {
+            if (itVector < endVector && !(*itVector).empty() && !isHyphenDigit(*itVector))
+            {
+                newVector = vectorSplit(*itVector, ":");
+                std::cout << "My first vector is: " << *(newVector.begin()) << std::endl;
+                std::cout << "My second vector is: " << *(++newVector.begin()) << std::endl;
+                firstIter = newVector.begin();
+                secondtIter = newVector.begin();
+                secondtIter++;
+                _formData.insert(std::pair<std::string, std::string>(*firstIter, *secondtIter));
+            }
+        }
+    }
+}
+
 /**
  * @brief MAPSPLIT
  * Nos envian un vector con un split por \n y un delimitador
@@ -36,7 +73,7 @@ std::vector<std::string>    vectorSplit(std::string file, std::string delimiter)
  * @param delimiter 
  * @return std::map<std::string, std::string> 
  */
-std::map<std::string, std::string> mapSplit(std::vector<std::string> auxVector, std::string delimiter)
+std::map<std::string, std::string> Request::mapSplit(std::vector<std::string> auxVector, std::string delimiter)
 {
     std::map<std::string, std::string> auxMap;
     std::vector<std::string> newVector;
@@ -59,6 +96,11 @@ std::map<std::string, std::string> mapSplit(std::vector<std::string> auxVector, 
                 *secondtIter = *secondtIter + delimiter + *(++secondtIter);
                 size++;
             }
+        }
+        if (isHyphenDigit(*itVector))
+        {
+            setFormData(itVector, auxVector, auxVector.end() -1);
+            return (auxMap);
         }
         auxMap.insert(std::pair<std::string, std::string>(*firstIter, *secondtIter));
         if (*itVector == "Connection: close")
@@ -167,8 +209,9 @@ int Request::errorsToken()
  */
 int Request::tokenRequest(void)
 {
-    std::string file = "DELETE /gfdgdf HTTP/1.1\nuser-agent: Thunder Client (https://www.thunderclient.com)\naccept: */*\nauthorization: 13245687\ncontent-type: application/json\ncontent-length: 23\naccept-encoding: gzip, deflate, br\nHost: 127.0.0.1:3001\nConnection: close\n{\n\"name\": \"Hola\"\n}; ";
+    // std::string file = "DELETE /gfdgdf HTTP/1.1\nuser-agent: Thunder Client (https://www.thunderclient.com)\naccept: */*\nauthorization: 13245687\ncontent-type: application/json\ncontent-length: 23\naccept-encoding: gzip, deflate, br\nHost: 127.0.0.1:3001\nConnection: close\n{\n\"name\": \"Hola\"\n}; ";
     // std::string file = "GET /favicon.ico HTTP/1.1\nHost: 127.0.0.1:8080\nConnection: keep-alive\nsec-ch-ua: \"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"\nsec-ch-ua-mobile: ?0\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36\nsec-ch-ua-platform: \"macOS\"\nAccept: image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8\nSec-Fetch-Site: same-origin\nSec-Fetch-Mode: no-cors\nSec-Fetch-Dest: image\nReferer: http://127.0.0.1:8080/\nAccept-Encoding: gzip, deflate, br\nAccept-Language: en-US,en;q=0.9,es;q=0.8";
+    std::string file = "POST / HTTP/1.1\nUser-Agent: PostmanRuntime/7.31.1\nAccept: */*\nPostman-Token: a440ce60-e830-46ec-924e-ca8d079f302d\nHost: 127.0.0.1:3002\nAccept-Encoding: gzip, deflate, br\nConnection: keep-alive\nContent-Type: multipart/form-data; boundary=--------------------------321951531579093893025774\nContent-Length: 145640\n\n----------------------------321951531579093893025774\nContent-Disposition: form-data; name=\"name\"\n\nhola\n----------------------------321951531579093893025774\nContent-Disposition: form-data; name=\"pass\"\n\nmundo\n----------------------------321951531579093893025774\nContent-Disposition: form-data; name=\"file\"; filename=\"code.png\"\nContent-Type: image/png";
     std::vector<std::string> lineVector;
 
     lineVector = vectorSplit(file, "\n");
@@ -184,6 +227,10 @@ int Request::tokenRequest(void)
     for (std::map<std::string, std::string>::iterator iterMap  = _header.begin(); iterMap != _header.end(); iterMap++)
     {
         std::cout << "MAP: " << iterMap->first << " & " << iterMap->second << std::endl;
+    }
+    for (std::map<std::string, std::string>::iterator iter  = _formData.begin(); iter != _formData.end(); iter++)
+    {
+        std::cout << "FORMDATA: " << iter->first << " & " << iter->second << std::endl;
     }
     std::cout << "BODY: " << _body << std::endl;
     return (errorsToken());
--- src/Request.hpp
+++ src/Request.hpp
@@ -10,15 +10,19 @@ class Request
     std::string                         _path;
     std::string                         _body;
     std::map<std::string, std::string>  _header;
+    std::map<std::string, std::string>  _formData;
 
-    public:
-    int     tokenRequest(void); 
+    private:
     int     errorsToken();
     void    setMethod(std::vector<std::string> lineVector);
     void    setHttp(std::vector<std::string> lineVector);
     void    setPath(std::vector<std::string> lineVector);
     void    setHeader(std::vector<std::string> lineVector);
+    void    setFormData(std::vector<std::string>::iterator itVector, std::vector<std::string> auxVector, std::vector<std::string>::iterator endVector);
     void    setBody(std::vector<std::string> lineVector);
+
+    std::map<std::string, std::string> mapSplit(std::vector<std::string> auxVector, std::string delimiter);
+
 
     /*      GETTERS     */
     std::string getMethod(void);
@@ -26,4 +30,7 @@ class Request
     std::string getPath(void);
     std::map<std::string, std::string>  getHeader(void);
     std::string getBody(void);
+
+    public:
+    int     tokenRequest(void); 
 };
