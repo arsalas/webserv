4116933a328c459448c4f765cd9cd8479804098f
aliciamurma <aliciamurciamaya@gmail.com>
2023-03-25 19:24:39 +0100

    funciona Request

--- src/Http/Request.cpp
+++ src/Http/Request.cpp
@@ -223,9 +223,7 @@ void    Request::setContentDisposition(std::vector<std::string> lineVector)
                 cdIter = _ContentDisposition.end();
                 std::string str = *iter;
                 _ContentDisposition.insert(cdIter, str);
-                cdIter++;
             }
-            std::cout << "B\n";
             break ;
         }
     }
@@ -238,13 +236,7 @@ void    Request::setContentDisposition(std::vector<std::string> lineVector)
  */
 int Request::tokenRequest(std::string req)
 {
-    std::cout << "ENTRO DENTRO DE TOKEN REQUEST" << std::endl;
-    // std::string req = "DELETE /gfdgdf HTTP/1.1\nuser-agent: Thunder Client (https://www.thunderclient.com)\naccept: */*\nauthorization: 13245687\ncontent-type: application/json\ncontent-length: 23\naccept-encoding: gzip, deflate, br\nHost: 127.0.0.1:3001\nConnection: close\n{\n\"name\": \"Hola\"\n}; ";
-    // std::string file = "GET /favicon.ico HTTP/1.1\nHost: 127.0.0.1:8080\nConnection: keep-alive\nsec-ch-ua: \"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"\nsec-ch-ua-mobile: ?0\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36\nsec-ch-ua-platform: \"macOS\"\nAccept: image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8\nSec-Fetch-Site: same-origin\nSec-Fetch-Mode: no-cors\nSec-Fetch-Dest: image\nReferer: http://127.0.0.1:8080/\nAccept-Encoding: gzip, deflate, br\nAccept-Language: en-US,en;q=0.9,es;q=0.8";
-    // std::string file = "POST / HTTP/1.1\nUser-Agent: PostmanRuntime/7.31.1\nAccept: */*\nPostman-Token: a440ce60-e830-46ec-924e-ca8d079f302d\nHost: 127.0.0.1:3002\nAccept-Encoding: gzip, deflate, br\nConnection: keep-alive\nContent-Type: multipart/form-data; boundary=--------------------------321951531579093893025774\nContent-Length: 145640\n\n----------------------------321951531579093893025774\nContent-Disposition: form-data; name=\"name\"\n\nhola\n----------------------------321951531579093893025774\nContent-Disposition: form-data; name=\"pass\"\n\nmundo\n----------------------------321951531579093893025774\nContent-Disposition: form-data; name=\"file\"; filename=\"code.png\"\nContent-Type: image/png";
-    // std::string req = "GET /?name=hjkhjk&name2=hjkhjk HTTP/1.1\nHost: 127.0.0.1:3001\nConnection: keep-alive\nsec-ch-ua: \"Google Chrome\";v=\"111\", \"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"111\"\nsec-ch-ua-mobile: ?0\nsec-ch-ua-platform: \"macOS\"\nUpgrade-Insecure-Requests: 1\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\nSec-Fetch-Site: same-origin\nSec-Fetch-Mode: navigate\nSec-Fetch-User: ?1\nSec-Fetch-Dest: document\nReferer: http://127.0.0.1:3001/\nAccept-Encoding: gzip, deflate, br\nAccept-Language: en-US,en;q=0.9,es;q=0.8,ru;q=0.7";
     std::vector<std::string> lineVector;
-    // req = "GET / HTTP/1.1\nHost: localhost:8082\nConnection: keep-alive\nsec-ch-ua: \"Google Chrome\";v=\"111\", \"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"111\"\nsec-ch-ua-mobile: ?0\nsec-ch-ua-platform: \"macOS\"\nUpgrade-Insecure-Requests: 1\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\nSec-Fetch-Site: none\nSec-Fetch-Mode: navigate\nSec-Fetch-User: ?1\nSec-Fetch-Dest: document\nAccept-Encoding: gzip, deflate, br\nAccept-Language: en-US,en;q=0.9";
 
     lineVector = Strings::vectorSplit(req, "\n");
     setMethod(lineVector);
@@ -254,29 +246,25 @@ int Request::tokenRequest(std::string req)
     setHeader(lineVector);
     setBody(lineVector);
     setHostPort(lineVector);
-
-    std::cout << "METHOD: " << _method << std::endl;
-    std::cout << "HTTP: " << _http << std::endl;
-    std::cout << "PATH: " << _path << std::endl;
-    for (std::map<std::string, std::string>::iterator iterMap  = _header.begin(); iterMap != _header.end(); iterMap++)
-    {
-        std::cout << "MAP: " << iterMap->first << " & " << iterMap->second << std::endl;
-    }
-    for (std::map<std::string, std::string>::iterator iter  = _formData.begin(); iter != _formData.end(); iter++)
-    {
-        std::cout << "FORMDATA: " << iter->first << " & " << iter->second << std::endl;
-    }
-    std::cout << "BODY: " << _body << std::endl;
-    std::cout << "PORT: " << _port << std::endl;
-    
     setContentDisposition(lineVector);
-    std::cout << "DESPUES DE SET\n";
-    for (std::vector<std::string>::iterator iter; iter != _ContentDisposition.end(); iter++)
-    {
-        std::cout << "ContentDisposition is: " << *iter << std::endl;
-    }
-    // std::cout << _header.at("Content-Disposition") << std::endl;
 
+    // std::cout << "METHOD: " << _method << std::endl;
+    // std::cout << "HTTP: " << _http << std::endl;
+    // std::cout << "PATH: " << _path << std::endl;
+    // for (std::map<std::string, std::string>::iterator iterMap  = _header.begin(); iterMap != _header.end(); iterMap++)
+    // {
+    //     std::cout << "MAP: " << iterMap->first << " & " << iterMap->second << std::endl;
+    // }
+    // for (std::map<std::string, std::string>::iterator iter  = _formData.begin(); iter != _formData.end(); iter++)
+    // {
+    //     std::cout << "FORMDATA: " << iter->first << " & " << iter->second << std::endl;
+    // }
+    // std::cout << "BODY: " << _body << std::endl;
+    // std::cout << "PORT: " << _port << std::endl;
+    // for (std::vector<std::string>::iterator iter; iter != _ContentDisposition.end(); iter++)
+    // {
+    //     std::cout << "ContentDisposition is: " << *iter << std::endl;
+    // }
     return (errorsToken());
     return (0);
 }
