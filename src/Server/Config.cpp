4cc8ccaf76748d8e973aa69334eaea35aca48cc1
aliciamurma <aliciamurciamaya@gmail.com>
2023-03-30 17:18:58 +0200

    files

--- src/Server/Config.cpp
+++ src/Server/Config.cpp
@@ -6,7 +6,7 @@ Config::Config() : _parent(NULL)
 	// Request re;
 	// addListen(re.getPort());
 	// TODO quitar harcodeo
-	addListen(7002);
+	addListen(7000);
 	// addListen(3000);
 	// addListen(3001);
 	// addListen(3002);
